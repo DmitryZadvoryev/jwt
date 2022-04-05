d380005564c4cd490e996b35f886462230ca5557
Dmitry Zadvoryev <dmitryzadvoryev@gmail.com>
2022-04-05 22:38:53 +0300

    jwt

--- src/main/java/ru/jwt/JwtUtils.java
+++ src/main/java/ru/jwt/JwtUtils.java
@@ -13,24 +13,25 @@ import java.util.Date;
 @Component
 public class JwtUtils {
 
-    @Value("${app.jwtSecret}")
-    private String jwtSecret;
+    @Value("${security.jwt.secret")
+    private String secret;
 
-    @Value("${app.jwtExpirationMs}")
-    private int jwtExpirationMs;
+    @Value("${security.jwt.expiration")
+    private int expiration;
 
     public String generateJwtToken(Authentication authentication) {
-
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
-        return Jwts.builder().setSubject((userPrincipal.getUsername())).setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
     }
 
     public boolean validateJwtToken(String jwt) {
         try {
-            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(jwt);
-           // Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt);
+            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(jwt);
             return true;
         } catch (MalformedJwtException | IllegalArgumentException e) {
             System.err.println(e.getMessage());
@@ -40,7 +41,6 @@ public class JwtUtils {
     }
 
     public String getUserNameFromJwtToken(String jwt) {
-       // return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt).getBody().getSubject();
-        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(jwt).getBody().getSubject();
+        return Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(jwt).getBody().getSubject();
     }
 }
